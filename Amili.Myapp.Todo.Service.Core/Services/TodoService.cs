cac8a4b6906bf141adca58a25458d7aa2e505b7d
Risni Dheerasekara <risni.dheerasekara@visma.com>
2026-07-03 16:32:18 +0530

    Add ITodoService interface for creating todo items

---
+++ Amili.Myapp.Todo.Service.Core/Services/TodoService.cs
@@ -0,0 +1,11 @@
+
+
+using Amili.Myapp.Todo.Service.Core.Models.Request;
+using Amili.Myapp.Todo.Service.Core.Models.Response;
+
+namespace Amili.Myapp.Todo.Service.Core.Services;
+
+public interface ITodoService
+{
+    Task<TodoItemResponse> CreateTodoItemAsync(CreateTodoItemRequest request);
+}
