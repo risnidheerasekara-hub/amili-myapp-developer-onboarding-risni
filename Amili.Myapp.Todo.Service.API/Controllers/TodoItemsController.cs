d2639fa0e3c3a9a820fc5343419615f72592fe41
Risni Dheerasekara <risni.dheerasekara@visma.com>
2026-07-03 18:46:22 +0530

    Add TodoItemsController for managing todo item creation and retrieval

---
+++ Amili.Myapp.Todo.Service.API/Controllers/TodoItemsController.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Mvc;
+using Amili.Myapp.Todo.Service.Core.Models.Request;
+using Amili.Myapp.Todo.Service.Core.Models.Response;
+using Amili.Myapp.Todo.Service.Core.Services;
+
+namespace Amili.Myapp.Todo.Service.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class TodoItemsController(ITodoService todoService) : ControllerBase
+{
+
+    [HttpPost]
+    [ProducesResponseType(typeof(TodoItemResponse), StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<ActionResult> CreateTodoItem([FromBody] CreateTodoItemRequest request)
+    {
+        var response = await todoService.CreateTodoItemAsync(request);
+        return CreatedAtAction(nameof(GetTodoItemById), new { id = response.Id }, response);
+    }
+
+    [HttpGet("{id}")]
+    [ProducesResponseType(typeof(TodoItemResponse), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<ActionResult> GetTodoItemById(long id)
+    {
+        var response = await todoService.GetTodoItemByIdAsync(id);
+        if (response == null)
+        {
+            return NotFound();
+        }
+        return Ok(response);
+    }
+}
