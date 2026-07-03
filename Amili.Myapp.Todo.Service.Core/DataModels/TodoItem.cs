b280285240488af411b47f414e9604791c9a8ad4
Risni Dheerasekara <risni.dheerasekara@visma.com>
2026-07-03 16:31:40 +0530

    Add TodoItem data model class

---
+++ Amili.Myapp.Todo.Service.Core/DataModels/TodoItem.cs
@@ -0,0 +1,16 @@
+namespace Amili.MyApp.Todo.Service.Core.DataModels;
+
+public class TodoItem
+{
+    public long Id { get; set; }
+
+    public string Name { get; set; } = string.Empty;
+
+    public string? Description { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+
+    public bool IsCompleted { get; set; }
+
+    public DateTime? CompletedAt { get; set; }
+}
