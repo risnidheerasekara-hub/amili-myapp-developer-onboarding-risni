14c30222d8969a4b295d96679af47ab236bc161c
Risni Dheerasekara <risni.dheerasekara@visma.com>
2026-07-09 11:19:30 +0530

    empty repo

--- Amili.Myapp.Todo.Service.Core/Models/Request/UpdateTodoItemRequest.cs
+++
@@ -1,8 +0,0 @@
-namespace Amili.Myapp.Todo.Service.Core.Models.Request;
-
-public class UpdateTodoItemRequest
-{
-    public string? Name { get; set; }
-    public string? Description { get; set; }
-    public bool? IsCompleted { get; set; }
-}
